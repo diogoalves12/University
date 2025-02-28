abd11cf670fcdd18cbf4db588c0c9b33083d33f7
Diogo Alves <diogocalves26@gmail.com>
2025-02-28 16:21:32 +0000

    Add Py02 ex2

---
+++ Prog/Py02/area.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+int area(int base, int height){
+  return round(0.5 * base* height);
+}
+
+int area(int a, int b, int c){
+  double s = (a + b + c) / 2.0;
+  double A = sqrt(s * (s- a) * (s - b) * (s - c));
+  return round(A);
+}
+
+int main(){
+  return 0;
+}
